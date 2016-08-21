d194e006f3f966b40b5ae92fdc871983a05eb97f
Bogdan <bs2u13@soton.ac.uk>
2016-08-21 16:26:02 +0100

    Minor code changes, readme edited

--- FacebookBasketBaller/main.cpp
+++ FacebookBasketBaller/main.cpp
@@ -9,31 +9,33 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include "opencv2/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include <chrono>
-#include <thread>
-#include <string>
 
 using namespace cv;
 using namespace std;
-int lowR,lowG,lowB,highR,highG,highB;
+
+int lowR,highR;
 Mat image;
 Mat redImage;
 int counter;
+
+int delay10s = 4880;
 int delay20s = 1220;
 int delay30s = 4900;
 int delay40s = 4900;
+
 int delayFrame =1777;
+string adbPath = "/usr/local/adb";
 
 void getFeatures(int,void*) {
-    Mat bgr[3];   //destination array
-    split(image,bgr);//split source
+    Mat bgr[3];         //destination array
+    split(image,bgr);   //split source
 
-    namedWindow("R-red",CV_GUI_EXPANDED);
+    namedWindow("Thresholded",CV_GUI_EXPANDED);
+    
     threshold(bgr[0], bgr[0], lowR, highR, CV_THRESH_BINARY);
     redImage =bgr[0](Rect(0,225,bgr[0].cols,1776-225));
-    imshow("R-red",redImage);
+    
+    imshow("Thresholded",redImage);
 }
 
 void resetThrowsCallback(int state,void*){
@@ -68,56 +70,54 @@ int getProjectileLocation(Mat redImage){
 }
 
 void setupThresholdGUI(){
+    //Default thresholding values
     lowR=100;
     highR=255;
 
+    //Create trackbars
     createTrackbar("lowR", nullptr , &lowR, 255,getFeatures);
     createTrackbar("highR", nullptr , &highR, 255,getFeatures);
     createTrackbar("Counter", nullptr , &counter, 55,getFeatures);
-    createTrackbar("20's delay", nullptr , &delay20s, 5000,getFeatures);
-    createTrackbar("30's delay", nullptr , &delay30s, 25000,getFeatures);
-    createTrackbar("40's delay", nullptr , &delay40s, 25000,getFeatures);
-
+    createTrackbar("20's delay", nullptr , &delay20s, 10000,getFeatures);
+    createTrackbar("20's delay", nullptr , &delay20s, 10000,getFeatures);
+    createTrackbar("30's delay", nullptr , &delay30s, 10000,getFeatures);
+    createTrackbar("40's delay", nullptr , &delay40s, 10000,getFeatures);
+    
     createTrackbar("Interframe  delay", nullptr , &delayFrame, 5000,getFeatures);
     createButton("Reset throws",resetThrowsCallback,nullptr,CV_PUSH_BUTTON);
 
 }
 
 void fire(int startLocation,int endLocation){
-    string command = "/usr/local/adb shell input swipe "+ std::to_string(startLocation) + " 1557 "+ to_string(endLocation) +" 600 ";
+    string command = adbPath +" shell input swipe "+ std::to_string(startLocation) + " 1557 "+ to_string(endLocation) +" 600 ";
     system(command.data());
 }
 
 int main(int argc, const char * argv[]) {
-//    system("/usr/local/adb shell screencap -p | perl -pe 's/\\x0D\\x0A/\\x0A/g' > /Users/stbogdan/dev/FacebookBasketBaller/screen.png ");
-//    waitKey();
-    fire(0,0);
-
-
-    namedWindow("R-red",CV_GUI_EXPANDED);
-    auto start = chrono::steady_clock::now();
-
-    image = imread("/Users/stbogdan/dev/FacebookBasketBaller/screen.png", CV_LOAD_IMAGE_COLOR);
-
-    auto end = chrono::steady_clock::now();
-    auto diff = end - start;
-    cout<< chrono::duration <double, milli> (diff).count()<<"\tto get screenshot" <<endl;
-
-
+    if(argv[1] != nullptr){
+        adbPath = argv[1];
+    }
+    namedWindow("Thresholded",CV_GUI_EXPANDED);
     setupThresholdGUI();
-
     counter =0;
     waitKey();
-    string displayString = "";
+    string displayString ="";
+    string commandString ="";
+
     //Start firing loop
     while(true){
         //Get frame
-        start = chrono::steady_clock::now();
-        system("/usr/local/adb shell screencap -p /sdcard/screen.png");
-        system("/usr/local/adb pull /sdcard/screen.png /Users/stbogdan/dev/FacebookBasketBaller/screen.png");
-        image = imread("/Users/stbogdan/dev/FacebookBasketBaller/screen.png", CV_LOAD_IMAGE_COLOR);
+        auto start = chrono::steady_clock::now();
+        
+        commandString = adbPath + " shell screencap -p /sdcard/screen.png";
+        system(commandString.data());
+        
+        commandString = adbPath + " pull /sdcard/screen.png screenShot.png";
+        system(commandString.data());
+        
+        image = imread("screenShot.png", CV_LOAD_IMAGE_COLOR);
 
-        end = chrono::steady_clock::now();
+        auto end = chrono::steady_clock::now();
         double diff =chrono::duration <double, milli> (end - start).count();
         cout<< (int) diff <<"\tto get frame" <<endl;
 
@@ -138,13 +138,10 @@ int main(int argc, const char * argv[]) {
         end = chrono::steady_clock::now();
         diff =chrono::duration <double, milli> (end - start).count();
         cout<< (int) diff <<"\tto prepare for fire" <<endl;
+        
         //Fire if target found
-        if(counter==10) {
-            waitKey(4880);
-            cout<<"Waiting for target"<<endl;
-        }
-        else if(counter> 10 && counter<20){
-            waitKey(4880);
+        if(counter>= 10 && counter<20){
+            waitKey(delay10s);
             cout<<"Used 10's delay"<<endl;
 
         }
@@ -157,11 +154,11 @@ int main(int argc, const char * argv[]) {
             if(abs(basket-ball) > 100)
                 {
                     displayString = "Target out of range (" + to_string(abs(basket-ball)) +"), aborting fire";
-                    displayStatusBar("R-red", displayString) ;
-                    basket =-1;
+                    displayStatusBar("Thresholded", displayString) ;
+                    basket =-1; //This prevents the swipe from occuring
                 }
             else
-                displayStatusBar("R-red", "PRIMED FOR FIRE");
+                displayStatusBar("Thresholded", "Ready to fire, waiting for delay");
             waitKey(delay30s);
             cout<<"Used 30's delay"<<delay30s<<endl;
 
@@ -170,12 +167,11 @@ int main(int argc, const char * argv[]) {
             if(abs(basket-ball) > 100)
             {
                 displayString = "Target out of range (" + to_string(abs(basket-ball)) +"), aborting fire";
-                displayStatusBar("R-red", displayString) ;
-                basket =-1;
+                displayStatusBar("Thresholded", displayString) ;
+                basket =-1; //This prevents the swipe from occuring
             }
             else
-                displayStatusBar("R-red", "PRIMED FOR FIRE");
-
+                displayStatusBar("Thresholded", "Ready to fire, waiting for delay");
             waitKey(delay40s);
             cout<<"Used 40's delay "<<delay40s<<endl;
 
@@ -183,11 +179,11 @@ int main(int argc, const char * argv[]) {
         if(ball!= -1 && basket != -1){
             fire(ball,basket);
             counter++;
-            cout<<"Fired "<<counter<<endl;
+            cout<<"Fired for "<<counter<<endl;
 
         }
         else{
-            cout<<endl<<"FIRE ABORDED TARGET NOT FOUND "<<counter<<endl<<endl;
+            cout<<endl<<"Target not found, retrying for "<<counter<<endl;
             continue;
         }
 
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# FacebookBasket
-A script capable of playing the Facebook Messenger basketball minigame
+# Facebook Basketball Bot #
+A script capable of playing the Facebook Messenger basketball minigame (tested on the LG G2)
+
+## Dependencies ##
+- OpenCV 3.1
+- CMake (use to build the project)
+
+
+ ## How to use ##
+ - Connect phone with the game open (also ADB debugging enabled)
+ - Run the produced executable, adding the path to ADB as the first argument (default is "/usr/local/adb")
+ - Adjust delays if everything fails  
+ - Get a high score
