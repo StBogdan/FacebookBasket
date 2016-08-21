7c36e444cbf09b2ac1c7d0a9e0aea746597b0bea
Bogdan <bs2u13@soton.ac.uk>
2016-08-21 13:07:35 +0100

    Initial commit

--- FacebookBasketBaller/main.cpp
+++ FacebookBasketBaller/main.cpp
@@ -7,7 +7,7 @@
 //
 
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>f
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
@@ -20,6 +20,10 @@ int lowR,lowG,lowB,highR,highG,highB;
 Mat image;
 Mat redImage;
 int counter;
+int delay20s = 1220;
+int delay30s = 4900;
+int delay40s = 4900;
+int delayFrame =1777;
 
 void getFeatures(int,void*) {
     Mat bgr[3];   //destination array
@@ -36,13 +40,13 @@ void resetThrowsCallback(int state,void*){
 }
 int getTargetLocation(Mat redImage){
     int start = -1;
-    for(int i=0 ;i < redImage.rows/2;i++){
+    for(int i=340 ;i < redImage.rows/2;i++){
         for(int j=0; j< redImage.cols;j++)
             if( redImage.at<uchar>(i,j) ==0 && start == -1){
                 start = j;
             }
             else if (start != -1 && redImage.at<uchar>(i,j) == 255){
-                cout<<"Lenght of target is"<<j-start<<" found at "<<i<<" "<<j<<endl;
+                cout<<"Lenght of target is \t"<<j-start<<" found at \t"<<i<<" "<<j<<"\t Middle Target:"<<start+(j-start)/2<<endl;
                 return start+(j-start)/2;
             }
     }
@@ -55,7 +59,7 @@ int getProjectileLocation(Mat redImage){
         if(redImage.at<uchar>(1370,j) == 0 && start == -1)
             start = j;
         else if (redImage.at<uchar>(1370,j) == 255 && start != -1){
-            cout<<"Lenght of projectile is"<<j-start<<endl;
+            cout<<"Lenght of project'l is \t"<<j-start<<" found at \t"<<1370<<" "<<j<<"\t Middle Ball:"<<start+(j-start)/2<<endl;
             return start+(j-start)/2;
         }
     return -1;
@@ -68,6 +72,12 @@ void setupThresholdGUI(){
     
     createTrackbar("lowR", nullptr , &lowR, 255,getFeatures);
     createTrackbar("highR", nullptr , &highR, 255,getFeatures);
+    createTrackbar("Counter", nullptr , &counter, 55,getFeatures);
+    createTrackbar("20's delay", nullptr , &delay20s, 5000,getFeatures);
+    createTrackbar("30's delay", nullptr , &delay30s, 25000,getFeatures);
+    createTrackbar("40's delay", nullptr , &delay40s, 25000,getFeatures);
+
+    createTrackbar("Interframe  delay", nullptr , &delayFrame, 5000,getFeatures);
     createButton("Reset throws",resetThrowsCallback,nullptr,CV_PUSH_BUTTON);
 
 }
@@ -83,7 +93,7 @@ int main(int argc, const char * argv[]) {
     fire(0,0);
     
     
-    namedWindow("Phone feed",CV_GUI_EXPANDED);
+    namedWindow("R-red",CV_GUI_EXPANDED);
     auto start = chrono::steady_clock::now();
     
     image = imread("/Users/stbogdan/dev/FacebookBasketBaller/screen.png", CV_LOAD_IMAGE_COLOR);
@@ -96,6 +106,9 @@ int main(int argc, const char * argv[]) {
     setupThresholdGUI();
     
     counter =0;
+    waitKey();
+    string displayString = "";
+    //Start firing loop
     while(true){
         //Get frame
         start = chrono::steady_clock::now();
@@ -104,11 +117,9 @@ int main(int argc, const char * argv[]) {
         image = imread("/Users/stbogdan/dev/FacebookBasketBaller/screen.png", CV_LOAD_IMAGE_COLOR);
         
         end = chrono::steady_clock::now();
-        diff = end - start;
-        start = chrono::steady_clock::now();
-        cout<< chrono::duration <double, milli> (diff).count()<<"\tto get frame" <<endl;
+        double diff =chrono::duration <double, milli> (end - start).count();
+        cout<< (int) diff <<"\tto get frame" <<endl;
 
-        
         //Threshold
         getFeatures(0,nullptr);
 
@@ -117,34 +128,74 @@ int main(int argc, const char * argv[]) {
         int ball        =getProjectileLocation(redImage);
         
         end = chrono::steady_clock::now();
-        diff = end - start;
-        cout<< chrono::duration <double, milli> (diff).count()<<"\tto get features and points" <<endl;
+        diff =chrono::duration <double, milli> (end - start).count();
+        cout<< (int) diff <<"\tto get screenshow, features and points" <<endl;
         
+        //Handle time variation by waiting a bit
+        waitKey(1200-(int) diff);
+
+        end = chrono::steady_clock::now();
+        diff =chrono::duration <double, milli> (end - start).count();
+        cout<< (int) diff <<"\tto prepare for fire" <<endl;
         //Fire if target found
         if(counter==10) {
-            waitKey(6650);
-            cout<<endl<<"Waiting for target";
-        }
-        else if(counter> 10 && counter<=20){
             waitKey(4880);
+            cout<<"Waiting for target"<<endl;
         }
-        else if(counter>20 && counter<=30){
-            waitKey(3600);
+        else if(counter> 10 && counter<20){
+            waitKey(4880);
+            cout<<"Used 10's delay"<<endl;
+
         }
-        else if(counter>30){
-            return 0;
+        else if(counter>=20 && counter<30){
+            waitKey(delay20s);
+            cout<<"Used 20's delay"<<endl;
+
+        }
+        else if(counter>=30 && counter<=40){
+            if(abs(basket-ball) > 100)
+                {
+                    displayString = "Target out of range (" + to_string(abs(basket-ball)) +"), aborting fire";
+                    displayStatusBar("R-red", displayString) ;
+                    basket =-1;
+                }
+            else
+                displayStatusBar("R-red", "PRIMED FOR FIRE");
+            waitKey(delay30s);
+            cout<<"Used 30's delay"<<delay30s<<endl;
+
+        }
+        else if(counter>40){
+            if(abs(basket-ball) > 100)
+            {
+                displayString = "Target out of range (" + to_string(abs(basket-ball)) +"), aborting fire";
+                displayStatusBar("R-red", displayString) ;
+                basket =-1;
+            }
+            else
+                displayStatusBar("R-red", "PRIMED FOR FIRE");
+
+            waitKey(delay40s);
+            cout<<"Used 40's delay "<<delay40s<<endl;
+
         }
         if(ball!= -1 && basket != -1){
             fire(ball,basket);
             counter++;
-            cout<<endl<<"Fired "<<counter<<endl<<endl;
+            cout<<"Fired "<<counter<<endl;
+            
         }
         else{
             cout<<endl<<"FIRE ABORDED TARGET NOT FOUND "<<counter<<endl<<endl;
+            continue;
         }
+       
+        end = chrono::steady_clock::now();
+        diff =chrono::duration <double, milli> (end - start).count();
+        cout<< (int) diff <<"\tto fire" <<endl<<endl;
         
         //Wait for ball to respawn
-         waitKey(1500);
+         waitKey(delayFrame);
     }
     
     return 0;
